492823a8278d05e5cf1ae1472fc06f1b9e4f90b9
marquessam <samuelnmarques@gmail.com>
2025-03-05 19:08:59 +0800

    Create threats.js

---
+++ src/data/threats.js
@@ -0,0 +1,41 @@
+// Great threats data
+const threats = {
+  hearts: { // Beasts
+    ace: {
+      name: "Ancient Red Dragon",
+      description: "A colossal and fire-breathing dragon that terrorizes the land."
+    },
+    "2": {
+      name: "Giant Kraken",
+      description: "A massive sea creature that can sink ships with ease."
+    },
+    "3": {
+      name: "Dire Wolf Pack",
+      description: "A pack of supernatural wolves that hunt in unison."
+    },
+    // Add more entries based on your tables
+  },
+  diamonds: { // Mystical Threats
+    ace: {
+      name: "Archmage's Tower",
+      description: "A towering and magical structure filled with arcane knowledge."
+    },
+    // Add more entries
+  },
+  clubs: { // Political
+    ace: {
+      name: "Cult of the Void",
+      description: "A secretive cult that seeks to unleash an otherworldly entity."
+    },
+    // Add more entries
+  },
+  spades: { // Undead
+    ace: {
+      name: "Lich",
+      description: "An undead sorcerer with immense magical powers."
+    },
+    // Add more entries
+  }
+};
+
+export default threats;
